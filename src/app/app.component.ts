b1713017211b9bc32e69b568b6526deec7c3803a
HJuls2 <giuliobaronti@gmail.com>
2021-04-13 18:45:09 +0200

    share with friend feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
+const { Share } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(public platform: Platform) { }
+
+  public async share() {
+    await Share.share({
+      title: 'ITeribus',
+      text: 'Una App per semplificare la creazione di autocertificazioni per gli spostamenti.',
+      url: 'https://itineribus-832f3.web.app/',
+      dialogTitle: 'Condividi con un Amico'
+    });
+  }
+
+  public isMobile(): boolean {
+    return this.platform.is('android') || this.platform.is('ios');
+  }
 }
